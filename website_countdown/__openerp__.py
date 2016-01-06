b0e19d966c081052ad25982bb25ddddcb2ed9516
Turkesh Patel <turkesh.pate@medma.in>
2016-01-06 13:59:33 +0530

    ADD: added license.

--- website_countdown/__openerp__.py
+++ website_countdown/__openerp__.py
@@ -27,6 +27,7 @@
 {
     'name': 'Countdown',
     'version': '1.0',
+    "license": "AGPL-3",
     'category': 'Website',
     'summary': 'Website Countdown Widget',
     'description':"""Countdown""",
